098e4ca930ab1c581c0a0bd83dfc3c10018bf3f1
jlee7362 <jlee736222@gmail.com>
2025-04-09 20:35:04 +0900

    2단계, 명언 입력받기

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
+    private int lastId = 0;
+
 
     App(Scanner sc) {
         this.sc = sc;
@@ -12,14 +15,34 @@ public class App {
     public void run() {
         System.out.println("== Motivation 앱 실행 ==");
 
-        // 좌우 공백 제거: trim()
-        System.out.print("명령어 : ");
-        String cmd = sc.nextLine().trim();
-        
+        while (true){
+            lastId++;
+            // 좌우 공백 제거: trim()
+            System.out.print("명령어) ");
+            String cmd = sc.nextLine().trim();
 
-        if (cmd.equals("exit")){
-            System.out.println("== Motivation 앱 종료==");
+            if (cmd.equals("add")){
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("인물 : ");
+                String name = sc.nextLine().trim();
+                System.out.println(lastId+"번 명언이 등록되었습니다.");
+            }
+
+
+
+
+            if (cmd.equals("exit")){
+                System.out.println("== Motivation 앱 종료==");
+                break;
+            }
+
         }
 
+    }
+    private void add_cmd(String cmd, String name){
+        lastId++;
+
+        System.out.println(1+"번 명언이 등록되었습니다.");
     }
 }
